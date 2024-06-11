ae9b8a85646f1d4f564e2a3322f15189ce336b30
Joseph M. Ndegwa [Quality Assurance] <jndegwa@CO-OPBANK.CO.KE>
2024-06-11 17:17:32 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+const App = () => {
+  return (
+
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+
+    </BrowserRouter>
+
+  )
+}
+
+export default App
