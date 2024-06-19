4ee658a9c066d87308734d909990262d077326b8
Joseph M. Ndegwa [Quality Assurance] <jndegwa@CO-OPBANK.CO.KE>
2024-06-19 16:14:59 +0300

    register and login

--- api/index.js
+++ api/index.js
@@ -5,18 +5,17 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js"
 
 dotenv.config();
-//console.log(process.env.MONGO);
+console.log(process.env.JWT_SECRET);
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.DB_URL)
   .then(() => {
     console.log("Connected to MongoDB");
     console.log("Wippy, winnning...");
   })
   .catch((err) => {
-    console.log("Error: " + err);
+    console.log("Error: " + err );
   });
-
 
 const app = express();
 app.use(express.json());
@@ -27,3 +26,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
